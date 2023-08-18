b8c5c3aa722a488deb7d3350cb49b190fb4fa893
brobeson <brobeson@users.noreply.github.com>
2023-08-17 18:29:27 -0600

    wip on a find docker module

---
+++ FindDocker.cmake
@@ -0,0 +1,7 @@
+find_program(docker docker)
+
+# function(add_container tag_name)
+#   cmake_parse_arguments(docker "" "VERSION" "" ${ARGN})
+#   add_custom_command(COMMAND ${docker} --build --tag) 
+#   add_custom_target()
+# endfunction()

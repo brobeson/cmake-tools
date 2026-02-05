abcba7f609435f120cc86587141bbe276d487077
brobeson <brobeson@users.reply.github.com>
2026-02-04 22:36:18 -0700

    popping a git stash

--- BoostTest.cmake
+++ BoostTest.cmake
@@ -125,6 +125,11 @@ function(boost_discover_tests TARGET)
 
   # TODO Handle each of these, or remove them from the function and documentation.
   foreach(param IN ITEMS DISCOVERY_MODE TEST_SPEC DL_PATHS)
+  set(unhandled_args
+    TEST_SUFFIX WORKING_DIRECTORY TEST_LIST REPORTER OUTPUT_DIR OUTPUT_PREFIX OUTPUT_SUFFIX DISCOVERY_MODE
+    TEST_SPEC EXTRA_ARGS PROPERTIES DL_PATHS
+  )
+  foreach(param IN LISTS unhandled_args)
     if(_${param})
       message(AUTHOR_WARNING "${param} is not supported, yet.")
     endif()
